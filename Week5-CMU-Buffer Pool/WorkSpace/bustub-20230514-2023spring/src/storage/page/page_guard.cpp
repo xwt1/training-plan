c4e40915434077f01d195eb12c9275d599ea5ec0
xwt1 <3366316054@qq.com>
2023-09-20 13:19:23 +0000

    accompilsh the basic_page_guard(maybe auto BasicPageGuard::operator=(BasicPageGuard &&that) wrong),and pass weak gtest

--- Week5-CMU-Buffer Pool/WorkSpace/bustub-20230514-2023spring/src/storage/page/page_guard.cpp
+++ Week5-CMU-Buffer Pool/WorkSpace/bustub-20230514-2023spring/src/storage/page/page_guard.cpp
@@ -3,17 +3,51 @@
 
 namespace bustub {
 
-BasicPageGuard::BasicPageGuard(BasicPageGuard &&that) noexcept {}
+BasicPageGuard::BasicPageGuard(BasicPageGuard &&that) noexcept {
+    // this->bpm_ = that.bpm_;
+    that.bpm_ = nullptr;
+    // this->page_ = that.page_;
+    that.page_ = nullptr;
+    // this->is_dirty_ = that.is_dirty_;
+    that.is_dirty_ = false;
+}
 
-void BasicPageGuard::Drop() {}
+void BasicPageGuard::Drop() {
+    // auto drop_frame_id = this->bpm_->page_table_[this->page->page_id_];
+    if(this->bpm_ != nullptr && this->page_ != nullptr){
+        this->bpm_->UnpinPage(this->page_->GetPageId(),this->is_dirty_);
+    }
+    this->bpm_ = nullptr;
+    this->page_= nullptr;
+    this->is_dirty_ = false;
+}
 
-auto BasicPageGuard::operator=(BasicPageGuard &&that) noexcept -> BasicPageGuard & { return *this; }
+auto BasicPageGuard::operator=(BasicPageGuard &&that) noexcept -> BasicPageGuard & {
+    //这里我直接drop原先的页面,感觉不太对,他的注释光说要考虑有啥动作需要考虑，
+    //当原先的页面被覆盖的时候。
+    //但是目前动作貌似就drop一个，只好drop了,就很奇怪为啥这时候要条用Unpin
+    this->Drop();
+    // this->bpm_ = that.bpm_;
+    that.bpm_ = nullptr;
+    // this->page_ = that.page_;
+    that.page_ = nullptr;
+    // this->is_dirty_ = that.is_dirty_;
+    that.is_dirty_ = false;
+    return *this; 
+}
 
-BasicPageGuard::~BasicPageGuard(){};  // NOLINT
+BasicPageGuard::~BasicPageGuard(){
+    this->Drop();
+};  // NOLINT
 
-ReadPageGuard::ReadPageGuard(ReadPageGuard &&that) noexcept = default;
+ReadPageGuard::ReadPageGuard(ReadPageGuard &&that) noexcept = default{
+    this->guard_(std::move(that.guard_));
+};
 
-auto ReadPageGuard::operator=(ReadPageGuard &&that) noexcept -> ReadPageGuard & { return *this; }
+auto ReadPageGuard::operator=(ReadPageGuard &&that) noexcept -> ReadPageGuard & {
+    
+    return *this; 
+}
 
 void ReadPageGuard::Drop() {}
 
--- Week5-CMU-Buffer Pool/WorkSpace/bustub-20230514-2023spring/test/storage/page_guard_test.cpp
+++ Week5-CMU-Buffer Pool/WorkSpace/bustub-20230514-2023spring/test/storage/page_guard_test.cpp
@@ -1,3 +1,57 @@
+// //===----------------------------------------------------------------------===//
+// //
+// //                         BusTub
+// //
+// // page_guard_test.cpp
+// //
+// // Identification: test/storage/page_guard_test.cpp
+// //
+// // Copyright (c) 2015-2019, Carnegie Mellon University Database Group
+// //
+// //===----------------------------------------------------------------------===//
+
+// #include <cstdio>
+// #include <random>
+// #include <string>
+
+// #include "buffer/buffer_pool_manager.h"
+// #include "storage/disk/disk_manager_memory.h"
+// #include "storage/page/page_guard.h"
+
+// #include "gtest/gtest.h"
+
+// namespace bustub {
+
+// // NOLINTNEXTLINE
+// TEST(PageGuardTest, ENABLED_SampleTest) {
+//   const std::string db_name = "test.db";
+//   const size_t buffer_pool_size = 5;
+//   const size_t k = 2;
+
+//   auto disk_manager = std::make_shared<DiskManagerUnlimitedMemory>();
+//   auto bpm = std::make_shared<BufferPoolManager>(buffer_pool_size, disk_manager.get(), k);
+
+//   page_id_t page_id_temp;
+//   auto *page0 = bpm->NewPage(&page_id_temp);
+
+//   auto guarded_page = BasicPageGuard(bpm.get(), page0);
+
+//   EXPECT_EQ(page0->GetData(), guarded_page.GetData());
+//   EXPECT_EQ(page0->GetPageId(), guarded_page.PageId());
+//   EXPECT_EQ(1, page0->GetPinCount());
+
+//   guarded_page.Drop();
+
+//   EXPECT_EQ(0, page0->GetPinCount());
+
+//   // Shutdown the disk manager and remove the temporary file we created.
+//   disk_manager->ShutDown();
+// }
+
+// }  // namespace bustub
+
+
+
 //===----------------------------------------------------------------------===//
 //
 //                         BusTub
@@ -23,7 +77,32 @@
 namespace bustub {
 
 // NOLINTNEXTLINE
-TEST(PageGuardTest, DISABLED_SampleTest) {
+// TEST(PageGuardTest, SampleTest) {
+//   const std::string db_name = "test.db";
+//   const size_t buffer_pool_size = 5;
+//   const size_t k = 2;
+
+//   auto disk_manager = std::make_shared<DiskManagerUnlimitedMemory>();
+//   auto bpm = std::make_shared<BufferPoolManager>(buffer_pool_size, disk_manager.get(), k);
+
+//   page_id_t page_id_temp;
+//   auto *page0 = bpm->NewPage(&page_id_temp);
+
+//   auto guarded_page = BasicPageGuard(bpm.get(), page0);
+
+//   EXPECT_EQ(page0->GetData(), guarded_page.GetData());
+//   EXPECT_EQ(page0->GetPageId(), guarded_page.PageId());
+//   EXPECT_EQ(1, page0->GetPinCount());
+
+//   guarded_page.Drop();
+
+//   EXPECT_EQ(0, page0->GetPinCount());
+
+//   // Shutdown the disk manager and remove the temporary file we created.
+//   disk_manager->ShutDown();
+// }
+
+TEST(PageGuardTest, ReadTset) {
   const std::string db_name = "test.db";
   const size_t buffer_pool_size = 5;
   const size_t k = 2;
@@ -34,15 +113,30 @@ TEST(PageGuardTest, DISABLED_SampleTest) {
   page_id_t page_id_temp;
   auto *page0 = bpm->NewPage(&page_id_temp);
 
-  auto guarded_page = BasicPageGuard(bpm.get(), page0);
-
-  EXPECT_EQ(page0->GetData(), guarded_page.GetData());
-  EXPECT_EQ(page0->GetPageId(), guarded_page.PageId());
+  // test ~ReadPageGuard()
+  {
+    auto reader_guard = bpm->FetchPageRead(page_id_temp);
+    EXPECT_EQ(2, page0->GetPinCount());
+  }
   EXPECT_EQ(1, page0->GetPinCount());
 
-  guarded_page.Drop();
+  // test ReadPageGuard(ReadPageGuard &&that)
+  {
+    auto reader_guard = bpm->FetchPageRead(page_id_temp);
+    auto reader_guard_2 = ReadPageGuard(std::move(reader_guard));
+    EXPECT_EQ(2, page0->GetPinCount());
+  }
+  EXPECT_EQ(1, page0->GetPinCount());
 
-  EXPECT_EQ(0, page0->GetPinCount());
+  // test ReadPageGuard::operator=(ReadPageGuard &&that)
+  {
+    auto reader_guard_1 = bpm->FetchPageRead(page_id_temp);
+    auto reader_guard_2 = bpm->FetchPageRead(page_id_temp);
+    EXPECT_EQ(3, page0->GetPinCount());
+    reader_guard_1 = std::move(reader_guard_2);
+    EXPECT_EQ(2, page0->GetPinCount());
+  }
+  EXPECT_EQ(1, page0->GetPinCount());
 
   // Shutdown the disk manager and remove the temporary file we created.
   disk_manager->ShutDown();
