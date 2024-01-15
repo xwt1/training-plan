d70d70597d03e4839b070609b354dfe0ff1d1899
xwt1 <3366316054@qq.com>
2024-01-15 10:31:59 +0000

    add grade graph

---
+++ Week12-DataMining/src/main.py
@@ -0,0 +1,14 @@
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+# Plotting libraries
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(color_codes = True)
+for i in range(10):
+    print(10)
